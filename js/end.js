23abbb25394c8486d38c2f33379cd304b4c6163b
Austin Eng <213reeses@gmail.com>
2013-11-09 17:29:35 -0500

    end round bottom bar styling

--- js/end.js
+++ js/end.js
@@ -1,7 +1,11 @@
 function nextRoundCountdown(startVal) {
     $('#next-round-timer').html(startVal.toString());
     var downcount = setTimeout(function(){
-        $('#next-round-timer').html(Math.round((parseFloat($('#next-round-timer').html())-.1)*100)/100);
+        var newnum = (Math.round((parseFloat($('#next-round-timer').html())-.1)*100)/100).toString();
+        if (newnum.length==1) {
+            newnum=newnum+'.0';
+        }
+        $('#next-round-timer').html(newnum);
         if (parseFloat($('#next-round-timer').html())<=0) {
             console.log('Next Round Beginning!');
         } else {
