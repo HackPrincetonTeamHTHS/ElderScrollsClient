c1bcf8250dc3dac02d7640ddeca30075efc906a9
Austin Eng <213reeses@gmail.com>
2013-11-10 01:24:19 -0500

    [new] switch page js, combined js files

--- js/main.js
+++ js/main.js
@@ -57,6 +57,26 @@ function updateDial(time, max) {
     });
 }
 
+function nextRoundCountdown(time,complete) {
+    $('#next-round-timer').html(time/1000);
+    var start = new Date().getTime();
+    var interval = setInterval(function() {
+        var now = time-(new Date().getTime()-start);
+        if( now < 0) {
+            clearInterval(interval);
+            complete();
+        }
+        else updateMeter(now);
+    },10);
+}
+function updateMeter(time) {
+    var newnum = (Math.round(time/100)/10).toString();
+    if (newnum.length==1) {
+        newnum=newnum+'.0';
+    }
+    $('#next-round-timer').html(newnum);
+}
+
 var imagesource='http://businessnetworking.com/wp-content/uploads/2012/04/happy-face.jpg';
 function showPreview(img, time, callback) {
     $('#preview').attr('src', img);
@@ -109,4 +129,13 @@ function redirectToResults() {
 }
 $('#countdown-modal').modal('show');
 
-startGame(imagesource, 1000, 5000);
+function switchPage(id) {
+    var newp = $(document.getElementById(id));
+    newp.css('z-index',10).show();
+    $('.page-wraper').each(function() {
+        if($(this).attr('id')!=id) {
+            $(this).hide();
+        }
+    });
+    newp.css('z-index','');
+}
